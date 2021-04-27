19139fa7a737bfd9c2c6983d9056f10c95214d1d
plan5 <plan5 30434574+plan5@users.noreply.github.com>
2021-04-27 10:02:05 +0200

    Add tentative prepare script

---
+++ prepare.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+LOCAL="/home/root/.local"
+CONFIG="/home/root/.config/remarkable"
+
+echo "This will restructure your home directory."
+echo "You will have to rename $LOCAL/share-1 into $LOCAL/share"
+echo "if you want to use bare xochitl again."
+echo "Also your $CONFIG/xochitl.conf will be replaced by a symlink to $LOCAL/share"
+echo "To revert to the original state, move $LOCAL/share/xochitl.conf to $CONFIG, replacing the symlink."
+
+read -p "Do you understand that you are about to make changes to your system that may lead to data loss or other unforeseen behavior and that you are doing this on your own responsibility? (y/N)" response
+read -p "Do you wish to install this program?" yn
+    case $response in
+        [Yy]* ) dont_wreak_havoc(); break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+
+function dont_wreak_havoc(){
+    echo "This script is still under revision. Adapt line 15 to make it run."
+}
+
+function wreak_havoc(){
+    systemctl stop xochitl
+    
+    cd "$LOCAL"
+    mv "$CONFIG/xochitl.conf" "share/"
+    ln "$LOCAL/share/xochitl.conf" "$CONFIG/xochitl.conf"
+    
+    mv "share" "share-1"
+    mkdir "share" "share-2" "share-3"
+    chattr +i share
+    
+    systemctl enable remarvin.service --now
+}
